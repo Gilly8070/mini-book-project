4e854f0264a1740969751e3342377f9ba887acfb
Gulrez <sidd5312@gmailcom>
2022-01-17 01:28:11 +0530

    initial

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+    {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+    title: 'I love you to the moon',
+    author: 'Amelia HepWorth'
+    },
+    {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+    title: 'I love you to the moon',
+    author: 'Amelia HepWorth'
+    },
+    {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+    title: 'I love you to the moon',
+    author: 'Amelia HepWorth'
+    }
+]
